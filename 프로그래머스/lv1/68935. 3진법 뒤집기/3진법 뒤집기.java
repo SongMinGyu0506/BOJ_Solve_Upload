83323e5b82cc1ab75890a95b90322f9f6b13c47a
Song_MinGyu <40915234+SongMinGyu0506@users.noreply.github.com>
2023-09-30 16:03:22 +0900

    [level 1] Title: 3진법 뒤집기, Time: 8.69 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = "";
+        while(n != 0) {
+            str += n%3;
+            n = n/3;
+        }
+        return Integer.parseInt(str,3);
+    }
+}
